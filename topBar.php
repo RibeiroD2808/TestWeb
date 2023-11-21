ac7665c70bc5230016fae3a4a9b8b5068fae7b8b
RibeiroD2808 <diogo.ribeiro.2808@gmail.com>
2023-11-21 15:14:15 +0000

    new nav bar gallery is finished

---
+++ topBar.php
@@ -0,0 +1,16 @@
+<div class="topBar">
+        <div>
+            <img src="" alt="NameImage">
+        </div>
+        <div id="topBarPages">
+            <div>
+                <p>pag1</p>
+            </div>
+            <div>
+                <p>pag2</p>
+            </div>
+            <div>
+                <p>pag3</p>
+            </div>
+        </div>
+</div>
